bb4389dceed66af202524db7b712055842695286
vicalbiter <vic.albiter@gmail.com>
2020-12-15 20:19:48 -0600

    Prepare router views

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,34 +3,23 @@
   <div>
     <!-- Top Navbar Menu -->
     <div class="top-nav">
-      <b-navbar toggleable="md" type="dark" variant="info">
+      <b-navbar type="dark" variant="info">
         <b-navbar-brand href="#" @click="showDrawer = !showDrawer"><b-icon icon="list"></b-icon> Pink Workout</b-navbar-brand>
 
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <!-- Left aligned nav items -->
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+        </b-navbar-nav>
 
-        <b-collapse id="nav-collapse" is-nav>
-          <!-- Left aligned nav items -->
-          <b-navbar-nav>
-            <b-nav-item href="#">Link</b-nav-item>
-          </b-navbar-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
 
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto">
-            <b-nav-form>
-              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-            </b-nav-form>
+          <b-nav-item-dropdown text="Hello, User!" right>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
 
-            <b-nav-item-dropdown right>
-              <!-- Using 'button-content' slot -->
-              <template #button-content>
-                <em>User</em>
-              </template>
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
-              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-            </b-nav-item-dropdown>
-          </b-navbar-nav>
-        </b-collapse>
       </b-navbar>
     </div>
 
@@ -59,11 +48,8 @@
 
       <!-- Content -->
       <div class="main-screen w-100">
-        <div class="placeholder-content">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos magnam doloribus, nemo odio quas illum velit voluptates provident ratione, ipsa rerum! Veritatis tenetur eos harum, minima error repellat? Ad, quis eaque! Nihil, asperiores quae tempore, expedita alias assumenda, officia doloremque cum quod dolorem veritatis repudiandae dolores minus quisquam sequi cumque.
 
-        </div>
-        <!-- <router-view></router-view> -->
+        <router-view></router-view>
 
       </div>
 
@@ -97,10 +83,6 @@ body {
   background-color: #fff;
 }
 
-.showdrawer {
-  margin-left: 0;
-}
-
 .side-nav .logo {
   padding: .875rem 1.25rem;
   font-size: 1.2rem;
@@ -110,15 +92,15 @@ body {
   background-color:rgba(27,31,35,.05)
 }
 
+.showdrawer {
+  margin-left: 0;
+}
+
 /* Top Navbar */
 .top-nav {
 
 }
 
 /* Others */
-.placeholder-content {
-  margin: 20px
-}
-
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    Home
+  <div class="placeholder-content">
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis quod ex repudiandae assumenda fuga quasi, debitis distinctio voluptatum inventore. Dolor, sint. Vitae quo iure corporis, earum accusamus necessitatibus expedita distinctio suscipit provident dolores nostrum perferendis culpa? Voluptas ipsum earum sunt repudiandae deleniti recusandae nobis accusamus voluptatibus excepturi ea doloremque ratione incidunt labore in explicabo, doloribus animi itaque ipsam dolorum totam consectetur porro unde! Odio suscipit amet aperiam cupiditate atque nesciunt, nobis soluta? Optio fugiat corrupti, itaque quidem blanditiis hic aliquid nihil, est, harum vero dolor natus libero enim deserunt nemo ipsum veniam nostrum inventore aspernatur voluptate. Fugiat qui repellendus voluptate est excepturi quo assumenda necessitatibus iure recusandae perspiciatis modi sit nesciunt totam, accusantium quibusdam sapiente ipsa et? Maxime illum molestiae dolorum provident alias tempora, eligendi laboriosam sunt debitis quod repellat commodi vitae necessitatibus obcaecati ducimus exercitationem quos laudantium cupiditate laborum a iure voluptatibus! Doloribus iste obcaecati fuga quisquam necessitatibus architecto?
+
   </div>
 </template>
 
@@ -14,3 +15,9 @@ export default {
   }
 }
 </script>
+
+<style>
+.placeholder-content {
+  margin: 20px
+}
+</style>
