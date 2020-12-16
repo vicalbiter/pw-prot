003d6b46e4f653eac5a40e892aa36686a7df160a
vicalbiter <vic.albiter@gmail.com>
2020-12-15 19:32:39 -0600

    Change order of navs

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,65 +1,70 @@
 <template>
-  <div class="d-flex">
-    <!-- Side Bar -->
-    <div id="side-drawer" class="side-nav d-flex flex-column" :class="{ showdrawer: showDrawer }">
-      <div class="logo">PinkWorkout</div>
 
-      <div class="side-nav-menu">
-        <b-list-group class="list-group-flush">
-          <b-list-group-item active>Active</b-list-group-item>
-          <b-list-group-item>Link</b-list-group-item>
-          <b-list-group-item>Another Link</b-list-group-item>
-          <b-list-group-item disabled>Disabled</b-list-group-item>
-        </b-list-group>
-      </div>
+  <div>
+    <!-- Top Navbar Menu -->
+    <div class="top-nav">
+      <b-navbar toggleable="md" type="dark" variant="info">
+        <b-navbar-brand href="#" @click="showDrawer = !showDrawer"><b-icon icon="list"></b-icon> Pink Workout</b-navbar-brand>
 
-      <div class="side-nav-footer mt-auto">
-        <b-list-group class="list-group-flush">
-          <b-list-group-item>Link</b-list-group-item>
-          <b-list-group-item>Another Link</b-list-group-item>
-        </b-list-group>
-      </div>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <!-- Left aligned nav items -->
+          <b-navbar-nav>
+            <b-nav-item href="#">Link</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+            </b-nav-form>
+
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template #button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
 
     <!-- Main Content -->
-    <div class="main-screen w-100">
-      <!-- Top Navbar -->
-      <div class="top-nav">
-        <b-navbar toggleable="md" type="dark" variant="info">
-          <b-navbar-brand href="#" @click="showDrawer = !showDrawer"><b-icon icon="list"></b-icon> NavBar</b-navbar-brand>
+    <div class="d-flex">
 
-          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <!-- Side Navbar Menu -->
+      <div id="side-drawer" class="side-nav d-flex flex-column" :class="{ showdrawer: showDrawer }">
 
-          <b-collapse id="nav-collapse" is-nav>
-            <!-- Left aligned nav items -->
-            <b-navbar-nav>
-              <b-nav-item href="#">Link</b-nav-item>
-            </b-navbar-nav>
+        <div class="side-nav-menu">
+          <b-list-group class="list-group-flush">
+            <b-list-group-item active>Active</b-list-group-item>
+            <b-list-group-item>Link</b-list-group-item>
+            <b-list-group-item>Another Link</b-list-group-item>
+            <b-list-group-item disabled>Disabled</b-list-group-item>
+          </b-list-group>
+        </div>
 
-            <!-- Right aligned nav items -->
-            <b-navbar-nav class="ml-auto">
-              <b-nav-form>
-                <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-                <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-              </b-nav-form>
-
-              <b-nav-item-dropdown right>
-                <!-- Using 'button-content' slot -->
-                <template #button-content>
-                  <em>User</em>
-                </template>
-                <b-dropdown-item href="#">Profile</b-dropdown-item>
-                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-              </b-nav-item-dropdown>
-            </b-navbar-nav>
-          </b-collapse>
-        </b-navbar>
+        <div class="side-nav-footer mt-auto">
+          <b-list-group class="list-group-flush">
+            <b-list-group-item>Link</b-list-group-item>
+            <b-list-group-item>Another Link</b-list-group-item>
+          </b-list-group>
+        </div>
       </div>
 
-      <div class="content">
+      <!-- Content -->
+      <div class="main-screen w-100">
+        <div class="placeholder-content">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos magnam doloribus, nemo odio quas illum velit voluptates provident ratione, ipsa rerum! Veritatis tenetur eos harum, minima error repellat? Ad, quis eaque! Nihil, asperiores quae tempore, expedita alias assumenda, officia doloremque cum quod dolorem veritatis repudiandae dolores minus quisquam sequi cumque.
+        </div>
+        <!-- <router-view></router-view> -->
 
       </div>
-      <!-- <router-view></router-view> -->
 
     </div>
 
@@ -70,17 +75,21 @@
 export default {
   data() {
     return {
-      showDrawer: false
+      showDrawer: true
     }
   }
 }
 </script>
 
 <style>
+body {
+  background-color: #f9f9f9;
+}
+
 .side-nav {
-  min-height: 100vh;
-  width: 12rem;
-  margin-left: -12rem;
+  min-height: calc(100vh - 56px);
+  width: 14rem;
+  margin-left: -14rem;
   transition: margin .25s ease-in-out;
   background-color: #fff;
 }
@@ -97,8 +106,9 @@ export default {
 
 }
 
-body {
-  background-color: #fff;
+.placeholder-content {
+  margin: 20px
 }
+
 
 </style>
