ac3494653901080fa4bbc75ca84df8e9030c0bd4
vicalbiter <vic.albiter@gmail.com>
2020-12-26 11:49:15 -0600

    Add Videos component

--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="container">
     <b-card-group>
-      <b-card no-body>
+      
+      <!-- Card with iframe -->
+      <!-- <b-card no-body>
         <b-card-header>
-          <iframe width="100%" height="300" src="https://www.youtube.com/embed/HduvmoInBgk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <div class="iframe-container">
+            <iframe src="https://player.vimeo.com/video/118897935?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen class="responsive-iframe"></iframe>
+          </div>
         </b-card-header>
         <b-card-body>
           Hola
         </b-card-body>
+      </b-card> -->
+
+      <b-card>
+        <b-card-header></b-card-header>
       </b-card>
     </b-card-group>
   </div>
@@ -20,5 +28,21 @@ export default {
 </script>
 
 <style>
+.iframe-container {
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  padding-top: 56.25%; /* 16:9 Aspect Ratio (divide 9 by 16 = 0.5625) */
+}
 
+/* Then style the iframe to fit in the container div with full height and width */
+.responsive-iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+}
 </style>
